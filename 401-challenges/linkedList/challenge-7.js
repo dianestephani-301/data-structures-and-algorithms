76d29c5954f6fd23606c0e534d1d06a495dec042
dianestephani <diane.stephani@gmail.com>
2020-12-28 18:07:57 -0800

    instructions printed

---
+++ 401-challenges/linkedList/challenge-7.js
@@ -0,0 +1,3 @@
+'use strict';
+
+//Write a method for the Linked List class which takes a number, k, as a parameter. Return the node’s value that is k from the end of the linked list. You have access to the Node class and all the properties on the Linked List class as well as the methods created in previous challenges.
